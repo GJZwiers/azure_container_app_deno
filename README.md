44856e88a665c655e98dade0aa9343fe37030843
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-06-15 15:13:02 +0200

    feat: add logger

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 (work in progress)
 
-This repository contains an Azure Container App running a Deno image, deployed using Azure Bicep.
+This repository contains an Azure Container App running a Deno image, deployed
+using Azure Bicep.
--- src/mod.ts
+++ src/mod.ts
@@ -1,9 +1,11 @@
-import { serve } from "https://deno.land/std@0.134.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.143.0/http/server.ts";
+import * as log from "https://deno.land/std@0.143.0/log/mod.ts";
 
 function handler(_req: Request): Response {
-    return new Response("Hello, World!");
-  }
+  log.info("Received request.");
+  return new Response("Hello, World!");
+}
 
 serve(handler, { port: 8080 });
 
-console.log("Request handled.");
+log.info("Server running on port 8080.");
