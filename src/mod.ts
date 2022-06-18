58b953855af6c5b9bf5c09071ae704848259ed42
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-06-18 21:37:43 +0200

    chore: add health endpoint

--- src/mod.ts
+++ src/mod.ts
@@ -3,10 +3,17 @@ import { opine } from "https://deno.land/x/opine@2.2.0/mod.ts";
 const app = opine();
 
 // deno-lint-ignore require-await
-app.get("/", async function (req, res) {
-  console.log(req);
-  
+app.get("/", async (req, res) => {
   res.send("Hello World");
+});
+
+app.get("/point", (req, res) => {
+  const time = performance.timeOrigin + performance.now();
+  res.send(time);
+});
+
+app.get("/health", (req, res) => {
+  res.sendStatus(200);
 });
 
 app.listen(
