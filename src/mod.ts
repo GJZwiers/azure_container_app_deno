c94182d8e0b48311b321a88390890b9f10846498
Geert-Jan Zwiers <geertjanzwiers@protonmail.com>
2022-06-19 10:15:31 +0200

    chore: allow different port through env

--- src/mod.ts
+++ src/mod.ts
@@ -2,21 +2,19 @@ import { opine } from "https://deno.land/x/opine@2.2.0/mod.ts";
 
 const app = opine();
 
+const port = parseInt(Deno.env.get("PORT") ?? "8080");
+
 // deno-lint-ignore require-await
-app.get("/", async (req, res) => {
+app.get("/", async (_req, res) => {
   res.send("Hello World");
 });
 
-app.get("/point", (req, res) => {
-  const time = performance.timeOrigin + performance.now();
-  res.send(time);
-});
-
-app.get("/health", (req, res) => {
+app.get("/health", (_req, res) => {
+  // TODO: add a health check
   res.sendStatus(200);
 });
 
 app.listen(
-  8080,
-  () => console.log("server has started on http://localhost:8080 🚀"),
+  port,
+  () => console.log(`Server has started on http://localhost:${port} 🚀`),
 );
